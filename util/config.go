272ce2241d80f14e45ef439e97415a5c51f30a2d
vuphan121 <ratayael420@gmail.com>
2025-07-15 17:03:42 +0700

    add file

--- util/config.go
+++ util/config.go
@@ -1,9 +1,6 @@
 package util
 
 import (
-	"crypto/tls"
-	"fmt"
-	"github.com/redis/go-redis/v9"
 	"log"
 	"os"
 	"strconv"
@@ -14,12 +11,6 @@ import (
 	"github.com/vuphan121/quotum/storage"
 )
 
-type RedisConfig struct {
-	Addr     string
-	Username string
-	Password string
-	DB       int
-}
 type Config struct {
 	Rate      int
 	Interval  time.Duration
@@ -27,7 +18,7 @@ type Config struct {
 	Logging   bool
 	Algorithm string
 	APIKey    string
-	Redis     *RedisConfig
+	RedisURL  string
 }
 
 var AppConfig Config
@@ -61,48 +52,13 @@ func init() {
 	}
 
 	var store storage.Storage
-	var redisCfg *RedisConfig
+	var redis_url string
 
 	switch strings.ToLower(storeType) {
 	case "memory":
 		store = storage.SharedMemoryStorage
 	case "redis":
-		addr := os.Getenv("REDIS_ADDR")
-		username := os.Getenv("REDIS_USERNAME")
-		password := os.Getenv("REDIS_PASSWORD")
-		dbStr := os.Getenv("REDIS_DB")
-		tlsEnabled := os.Getenv("REDIS_TLS") == "true"
-		db := 0
-		if dbStr != "" {
-			db, err = strconv.Atoi(dbStr)
-			if err != nil {
-				log.Fatalf("Invalid REDIS_DB: %v", err)
-			}
-		}
-
-		redisCfg = &RedisConfig{
-			Addr:     addr,
-			Username: username,
-			Password: password,
-			DB:       db,
-		}
-
-		redisOptions := &redis.Options{
-			Addr:     redisCfg.Addr,
-			Username: redisCfg.Username,
-			Password: redisCfg.Password,
-			DB:       redisCfg.DB,
-		}
-
-		if tlsEnabled {
-			redisOptions.TLSConfig = &tls.Config{}
-			fmt.Println("[CONFIG] TLS enabled for Redis")
-		} else {
-			fmt.Println("[CONFIG] TLS not enabled for Redis")
-		}
-
-		redis_client := redis.NewClient(redisOptions)
-		store = storage.NewRedisStorage(redis_client)
+		redis_url = os.Getenv("REDIS_URL")
 	default:
 		log.Fatalf("Invalid STORE: %s ('memory' or 'redis')", storeType)
 	}
@@ -126,6 +82,6 @@ func init() {
 		Logging:   logging,
 		Algorithm: algorithm,
 		APIKey:    apiKey,
-		Redis:     redisCfg,
+		RedisURL:  redis_url,
 	}
 }
